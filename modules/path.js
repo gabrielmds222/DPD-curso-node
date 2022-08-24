a488f27472c4121d9cb167e4da01cccbb652c20a
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-08-23 23:29:52 -0300

    feat: Aplica outros exemplos com o modulo path: dirname extname parse join

--- modules/path.js
+++ modules/path.js
@@ -1,4 +1,16 @@
 const path = require("path");
 
-// Basename
+// Basename -> Apenas o nome do arquivo atual
 console.log(path.basename(__filename));
+
+//dirname -> Nome do diretório atual
+console.log(path.dirname(__filename));
+
+// extname -> Extensão do arquivo
+console.log(path.extname(__filename));
+
+//parse -> Cria objeto path
+console.log(path.parse(__filename));
+
+//join -> Junta caminhos de arquivos
+console.log(path.join(__dirname, "teste", "teste2.html"));
