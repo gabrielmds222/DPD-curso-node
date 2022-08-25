7cfffd576dadeef5d6070ba40a289c937982f1c9
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-08-24 23:53:29 -0300

    feat: Utiliza método writeFile do módulo fs para criar arquivo dentro da pasta

--- modules/fs.js
+++ modules/fs.js
@@ -9,3 +9,16 @@ fs.mkdir(path.join(__dirname, "/test"), (error) => {
 
   console.log("pasta criada com sucesso");
 });
+
+// writeFile -> Criar um arquivo
+fs.writeFile(
+  path.join(__dirname, "test", "test.txt"),
+  "Hello node!",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ", error);
+    }
+
+    console.log("Arquivo criado com sucesso");
+  }
+);
